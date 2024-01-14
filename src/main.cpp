72e27983aeba2dd3a34d3c1cf2c94da3e2c524bb
medcelerate <evan.clark.professional@gmail.com>
2024-01-13 19:18:26 -0500

    Release now builds and loads correctly

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,26 @@
 
 VDJ_EXPORT HRESULT VDJ_API DllGetClassObject(const GUID& rclsid, const GUID& riid, void** ppObject)
 {
+	char* buffer = nullptr;
+	size_t size = 0;
+	_dupenv_s(&buffer, &size, "USERPROFILE");
+	
+	if (buffer == nullptr)
+	{
+		return CLASS_E_CLASSNOTAVAILABLE;
+	}
+
+	std::string userpath (buffer);
+
+	std::string path = userpath + "\\AppData\\Local\\VirtualDJ\\Plugins64\\VideoEffect\\TouchEngine.dll";
+
+	HMODULE hProcDLL = LoadLibraryA(path.c_str());
+
+	if (hProcDLL == nullptr)
+	{
+		return CLASS_E_CLASSNOTAVAILABLE;
+	}
+
 	if (memcmp(&rclsid, &CLSID_VdjPlugin8, sizeof(GUID)) == 0 && memcmp(&riid, &IID_IVdjPluginVideoFx8, sizeof(GUID)) == 0)
 	{
 		*ppObject = new VDJTouchEngine();
